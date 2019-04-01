8855f8a891ab7d9cbaee19536919bea861a704f8
alex fox <to.alexfox@gmail.com>
2019-04-01 19:02:00 +0200

    added bootstrap

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-import {Player} from "./src/players"
-import {Pond} from "./src/pond"
+// import {Player} from "./src/players"
+// import {Pond} from "./src/pond"
 
+var playerModal = document.getElementById("player-modal")
+
+window.onclick = function(event) {
+  if (event.target == playerModal) {
+    playerModal.style.display = "none";
+		console.log(playerModal)
+  }
+}
